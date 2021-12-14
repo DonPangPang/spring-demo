b9f2b1b62ac69513645d27afdb4d44b5c0bb8d9a
DonPangPang <powertle@outlook.com>
2021-12-14 21:24:42 +0800

    first commit

---
+++ src/main/java/cn/powertle/demo/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package cn.powertle.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/world")
+    public String sayHi(){
+        return "Hello World";
+    }
+}
